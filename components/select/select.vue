85a8f1530a1987ca36f0c55740a340fee70a1c06
echaoo <1299529160@qq.com>
2017-11-11 23:52:14 +0800

    add util.js && part of select.vue

---
+++ components/select/select.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="`${prefixCls}`" style="width: 120px;">
+    <div :class="classes" role="combobox" aria-autocomplete="list" aria-haspopup="true" aria-expanded="false"
+         tabindex="0">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  import {isMultipleOrTags} from './util'
+  export default {
+    props: {
+      prefixCls: {
+        type: String,
+        default: 'ant-select'
+      }
+    },
+    computed: {
+      classes () {
+        return [
+          `${this.prefixCls}-selection`,
+          `${this.prefixCls}-selection--${this.multiple ? 'multiple' : 'single'}`
+        ]
+      },
+      multiple () {
+        return isMultipleOrTags(this.props)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
